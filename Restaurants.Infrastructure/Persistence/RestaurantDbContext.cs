c62f859e87d0822c2f914547f976add19eaaecf6
Ahmed-Aboulbichr <aboulbichrahmed2@gmail.com>
2024-11-27 23:49:42 +0100

    configure ef in infrastructure with migrations

---
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantDbContext : DbContext
+{
+
+    internal DbSet<Restaurant> Restaurants { get; set; }
+
+    internal DbSet<Dish> Dishes;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=MON-DESKTOP;Database=RestaurantsDb;Integrated Security=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(r => r.RestaurantId);
+    }
+}
