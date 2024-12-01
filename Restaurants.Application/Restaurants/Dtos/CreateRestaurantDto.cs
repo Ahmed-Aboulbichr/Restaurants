020c81af0b8d861b0ccd8dfacb402a3c425d7062
Ahmed-Aboulbichr <aboulbichrahmed2@gmail.com>
2024-12-01 21:33:09 +0100

    model validation

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,23 +1,23 @@
-﻿using Restaurants.Application.Dishes.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
+    [StringLength(100, MinimumLength =3)]
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
+    [Required(ErrorMessage ="Insert a valid category")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
+    [Phone(ErrorMessage = "Please provide a valid phone number")]
     public string? ContactNumber { get; set; }
+    [EmailAddress(ErrorMessage ="Please provide a valid email address")]
     public string? ContactEmail { get; set; }
 
     public string? City { get; set; }
     public string? Street { get; set; }
+    [RegularExpression(@"^\d{2}-d{3}$", ErrorMessage ="Please provide a valid postal code XX-XXX")]
     public string? PostalCode { get; set; }
 }
